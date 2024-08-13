403bdf4a72b9a7429dc5460e117826c5e6ad22e4
Victório <victoriobastelli@gmail.com>
2024-08-12 22:42:42 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function tocaSom (idElemento) {
+    document.querySelector(idElemento).play()
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+}
